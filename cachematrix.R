f8b1a8f049c43dc1ff1295e2972d4b72b4fd487e
Jianliang Jin <jinfamily7@users.noreply.github.com>
2014-06-22 16:29:21 -0500

    Update cachematrix.R
    the first function creates a list that stores a matrix and its inverse, the second function returns the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
+## the first function creates a list that stores a matrix and its inverse, the second function returns the inverse of the matrix
 ## functions do
 
-## Write a short comment describing this function
+## this function creats a list that stores an invertible numeric square matrix 'x' and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+##  this function calculates the inverse of a square matrix 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       a <- makeCacheMatrix(x)
+       m <- a$getinv()
+       if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+       }
+       data <- a$get()
+       m <- solve(data)
+       a$setinv(m)
+       m             ## Return a matrix that is the inverse of 'x'
 }
