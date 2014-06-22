2656a3eb6dc32e257fda7dc63a6bbfb5d64f7d14
Jianliang Jin <jinfamily7@users.noreply.github.com>
2014-06-22 17:38:27 -0500

    Update cachematrix.R
    the first function creates a list that stores a matrix and its inverse, the second function returns the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## the first function creates a list that stores a matrix and its inverse, the second function returns the inverse of the matrix
-## functions do
+
 
 ## this function creats a list that stores an invertible numeric square matrix 'x' and its inverse
 
@@ -9,9 +9,10 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setinv <- function(inv) m <<- inv
-        getinv <- function() m
+        inv <- solve(x)                       ##inv is the inverse of the square matrix x
+        get <- function() x                   ##this function gets the matrix x
+        setinv <- function(inv) m <<- inv     ##this function sets the inverse of x
+        getinv <- function() m                ##this function gets the inverse of x
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
@@ -19,13 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ##  this function calculates the inverse of a square matrix 'x'
 cacheSolve <- function(x, ...) {
        a <- makeCacheMatrix(x)
-       m <- a$getinv()
+       m <- a$getinv()         ##this gets the inverse of the matrix x and sends it to m
        if(!is.null(m)) {
-            message("getting cached data")
-            return(m)
+           message("getting cached data")
+           return(m)
        }
-       data <- a$get()
-       m <- solve(data)
-       a$setinv(m)
-       m             ## Return a matrix that is the inverse of 'x'
+       data <- a$get()         ##this finds the matrix x and sends x to the variable data
+       m <- solve(data)        ##this finds the inverse of the matrix x
+       a$setinv(m)             ##this updates the inverse of x in the setinv function
+       m                       ##output the inverse of x to the cacheSolve function
 }
